4b18d435a788f3d0d7f7825d3a7c3dc49c5977a5
adamszeq <116906102+adamszeq@users.noreply.github.com>
2023-06-02 14:20:28 +0100

    update market_value_by_earnings_per_share.py

--- src/earnings_per_share/market_value_by_earnings_per_share.py
+++ src/earnings_per_share/market_value_by_earnings_per_share.py
@@ -1,67 +1,16 @@
-import pandas as pd
-from urllib.request import urlopen
-import certifi
 import json
-import numpy as np
 from pprint import pprint
+from urllib.request import urlopen
+
+import certifi
+import numpy as np
+import pandas as pd
+
 
 class AnnualEPS(): 
         
         def __init__(self) -> None:
-              pass
-          
-        def get_eps(self,datasets,ticker_column,ticker_data):
-            data = pd.read_csv(datasets)
-            eps_dict_list=[]
-            for ticker_id in data[ticker_column]:
-                url = f'https://financialmodelingprep.com/api/v3/income-statement/{ticker_id}?&apikey=764a0c82850f17c8235116b78792d7e1'
-                response = urlopen(url, cafile=certifi.where())
-                data = response.read().decode("utf-8")
-                if earnings_info_data := json.loads(data):
-                    earnings_info = earnings_info_data
-                period=(1/len(earnings_info)) if len(earnings_info) != 0 else 0
-                first_info=earnings_info[0]
-                first_eps=first_info.get('epsdiluted')
-                last_info=earnings_info[-1]
-                last_eps=last_info.get('epsdiluted')
-                eps_growth_diff = (first_eps/last_eps) if last_eps != 0 else 0
-                if eps_growth_diff < 0:
-                   eps_growth_diff_time= -(abs(eps_growth_diff)**period)
-                else:
-                   eps_growth_diff_time= (eps_growth_diff**period)
-                eps_growth_diff_time_minus_one=eps_growth_diff_time-1
-                eps_growth_rate=eps_growth_diff_time_minus_one
-                eps_data = {ticker_id: eps_growth_rate * 100}
-                eps_dict_list.append(eps_data)
-
-        def get_eps_pctChange(self,datasets,ticker_column,ticker_data):
-                data = pd.read_csv(datasets)
-                eps_dict_list = []
-                eps_figures = []
-                for ticker_id in data[ticker_column]:
-                        url = f'https://financialmodelingprep.com/api/v3/income-statement/{ticker_id}?&apikey=764a0c82850f17c8235116b78792d7e1'
-                        response = urlopen(url, cafile=certifi.where())
-                        data = response.read().decode("utf-8")
-                        if earnings_info_data := json.loads(data):
-                            earnings_info_bulk = earnings_info_data
-                        eps=np.array([data ['epsdiluted'] for data in earnings_info_bulk])
-
-                        eps_quantiled = eps[(eps>np.quantile(eps,0.05)) & (eps<np.quantile(eps,0.95))].tolist()
-
-                        eps_quantiled.reverse()
-
-                        eps_change = (pd.Series(eps_quantiled).pct_change().replace([np.inf, -np.inf], np.nan).mean())*100
-                        eps_coeffVar = abs(np.std(eps_quantiled))/ abs(np.mean(eps_quantiled))
-                        eps_data = {ticker_id: (eps_change , eps_coeffVar)}
-                        eps_dict_list.append(eps_data)
-                        # print(eps)
-                        # #print(count)
-                        # print(ticker_id , eps_change)
-                        #print(eps_quantiled)
-                
-                # with open(ticker_data, 'w') as file:
-                #    file.write(json.dumps(eps_dict_list))
-
+            pass
                 
         def combiningEPSMarketValue(self,url,marketValueLink):
          
