e85e2cfd227c99ffc7209da9d4695943176fb529
adamszeq <116906102+adamszeq@users.noreply.github.com>
2023-06-02 17:46:07 +0100

    update config.ini and data_download.py

--- src/tools/data_download.py
+++ src/tools/data_download.py
@@ -1,3 +1,6 @@
+from operator import inv
+
+
 class DownloadDataFMP():
 
     def __init__(self, api_key, first_part_download_link, second_part_download_link, investment_universe_path, save_path):
@@ -7,18 +10,27 @@ class DownloadDataFMP():
         self.second_part_download_link = second_part_download_link
         self.investment_universe_path = investment_universe_path
         self.save_path = save_path
- 
+
     def get_full_download_link(self):
         return self.first_part_download_link 
     
 if __name__ == '__main__':
     import configparser
+    import os
+    import pandas as pd
 
     config = configparser.ConfigParser()
-    config.read('../config/config.ini')
-    # PRINT ALL SECTIONS
-    print(config.sections())
+
+    config_file_path = os.path.abspath(os.path.join(__file__, '..', '..', '..', 'config', 'config.ini'))
+    config.read(config_file_path)
 
     API_KEY = config['FMP DATA']['api_key']
-    print(API_KEY)
-# config\config.ini
+    FIRST_PART_DOWNLOAD_LINK = config['FMP DATA']['download_link_first_part']
+    LIMIT = config['FMP DATA']['limit']
+    second_part_download_link = "_ticker_?period=quarter&limit=_limit_&apikey=_apikey_"
+    investment_universe_path = r"\data\investment universe.csv"
+    print(pd.read_csv(investment_universe_path))
+
+
+
+
