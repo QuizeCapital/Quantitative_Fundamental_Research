3d2d6b2da23acea73387e64efc947f020d992a51
adamszeq <116906102+adamszeq@users.noreply.github.com>
2023-06-03 15:37:27 +0100

    update data_download.py

--- src/tools/data_download.py
+++ src/tools/data_download.py
@@ -11,7 +11,7 @@ class DownloadDataFMP():
 
     def __init__(self, first_part_download_link, second_part_download_link,
                 investment_universe_path, investment_universe_ticker, save_path, cols_to_download,
-                download_all_columns, save_file_type):
+                download_all_columns, save_file_type, data_type):
 
         self.first_part_download_link = first_part_download_link
         self.second_part_download_link = second_part_download_link
@@ -21,19 +21,25 @@ class DownloadDataFMP():
         self.cols_to_download = cols_to_download
         self.download_all_columns = download_all_columns
         self.save_file_type = save_file_type
+        self.data_type = data_type
 
     def read_investment_universe(self):
-        return pd.read_csv(self.investment_universe_path)
+        return pd.read_csv(self.investment_universe_path).head(10)
     
     def get_full_download_link(self, ticker):
 
         self.ticker = ticker
-        second_part_download_link = self.second_part_download_link.replace(
-            '_ticker_', self.ticker) 
+
+        if self.data_type == 'index':
+            second_part_download_link = self.second_part_download_link
+
+        elif self.data_type == 'ticker':
+            second_part_download_link = self.second_part_download_link.replace(
+                '_ticker_', self.ticker) 
 
         return self.first_part_download_link + second_part_download_link
 
-    def download_data(self):
+    def download_ticker_data(self):
 
         df = pd.DataFrame()
 
@@ -49,7 +55,6 @@ class DownloadDataFMP():
                     
                     df = pd.concat([df, pd.DataFrame(data)])
 
-
         else:
             print('Downloading selected columns...')
             for count, ticker in enumerate(self.read_investment_universe()[self.investment_universe_ticker]):
@@ -64,17 +69,39 @@ class DownloadDataFMP():
 
         return df
 
-    
+    def download_index_data(self):
+
+        df = pd.DataFrame()
+
+        if self.download_all_columns == True:
+            print('Downloading all columns...')
+            full_link = self.get_full_download_link(ticker)
+
+            response = urlopen(full_link, cafile=certifi.where())
+            data = response.read().decode("utf-8")
+            if data := json.loads(data):
+                df = pd.concat([df, pd.DataFrame(data)])
+
+        else:
+            print('Downloading selected columns...')
+            full_link = self.get_full_download_link(ticker)
+
+            response = urlopen(full_link, cafile=certifi.where())
+            data = response.read().decode("utf-8")
+            if data := json.loads(data):  
+                df = pd.concat([df, pd.DataFrame(data)[self.cols_to_download]])
+
+        return df
     def save_data(self):
 
         if self.save_file_type == 'excel':
             print('Saving data to excel...')
-            df = self.download_data()
+            df = self.download_ticker_data()
             df.to_excel(self.save_path, index=False)
 
         elif self.save_file_type == 'parquet':
             print('Saving data to parquet...')
-            df = self.download_data()
+            df = self.download_ticker_data()
             df.to_parquet(self.save_path, index=False)
 
         return 'Data saved!'
@@ -99,13 +126,13 @@ if __name__ == '__main__':
     INVESTMENT_UNIVERSE_TICKER_COLUMN = config['FMP DATA']['investment_universe_ticker_column']
     cols_to_download = ['symbol', 'date', 'roic', 'priceToSalesRatio']
 
-    second_part_download_link = "historical-price-full/_ticker_?serietype=line&apikey=_apikey_"
+    second_part_download_link = "historical-price-full/_ticker_?apikey=_apikey_"
     second_part_download_link = second_part_download_link.replace('_apikey_', API_KEY)
     investment_universe_path = os.path.abspath(os.path.join(
         __file__, '..', '..', '..', 'data', 'investment universe.csv'))
     save_path = os.path.abspath(os.path.join(
-        __file__, '..', '..', '..', 'data', 'historical_price.parquet'))
+        __file__, '..', '..', '..', 'data', 'S&P_historical_price.parquet'))
 
     download_data = DownloadDataFMP(FIRST_PART_DOWNLOAD_LINK, second_part_download_link, investment_universe_path,
-                                    INVESTMENT_UNIVERSE_TICKER_COLUMN, save_path, cols_to_download, True, 'parquet')
+                                    INVESTMENT_UNIVERSE_TICKER_COLUMN, save_path, cols_to_download, True, 'parquet', 'index')
     print(download_data.save_data())
