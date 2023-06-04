d6041c47ecd6d5d33d3db55497b9752ad5839979
adamszeq <116906102+adamszeq@users.noreply.github.com>
2023-06-04 23:59:08 +0100

    update roic.py

--- src/profitability/roic.py
+++ src/profitability/roic.py
@@ -16,7 +16,19 @@ import pandas as pd
 
 
 
-class roic():
+class ROIC:
+    """
+    Class for calculating ROIC (Return on Invested Capital) related metrics.
+
+    Parameters:
+    - data_df_returns (pd.DataFrame): DataFrame containing return data
+    - data_df_ratios (pd.DataFrame): DataFrame containing ratio data
+    - date_column (str): Name of the column in the data DataFrame representing dates
+    - return_column (str): Name of the column in the return DataFrame representing returns
+    - symbol_column (str): Name of the column in both DataFrames representing symbols
+    - agg_function (str or callable): Aggregation function to use when calculating quintiles
+    - calc_column (str): Name of the column in the ratio DataFrame to calculate quintiles on
+    """
 
     def __init__(self, data_df_returns, data_df_ratios, date_column, return_column, symbol_column, agg_function, calc_column):
         self.data_df_returns = data_df_returns
@@ -28,41 +40,55 @@ class roic():
         self.calc_column = calc_column
 
     def get_symbols_in_each_quintile_ROIC(self):
+        """
+        Calculate the symbols in each quintile based on ROIC.
 
-        quintiles = QuintileFunctions(
-        self.data_df_ratios, self.date_column, self.symbol_column, self.calc_column, self.agg_function
-    ).get_quintile_groups()
-        
+        Returns:
+        - quintile_symbols (dict): Dictionary where keys represent quintiles and values are lists of symbols in each quintile
+        """
+        quintiles = QuintileFunctions(self.data_df_ratios, self.date_column, self.symbol_column, self.calc_column, self.agg_function).get_quintile_groups()
+
         quintile_symbols = {}
         for quintile, group in quintiles:
             quintile_symbols[quintile] = group[self.symbol_column].tolist()
 
         return quintile_symbols
-    
+
     def get_cagr(self):
+        """
+        Calculate the Compound Annual Growth Rate (CAGR) for each symbol.
 
-        return CAGR(
-            self.data_df_returns,
-            self.date_column,
-            self.return_column,
-            self.symbol_column,
-        ).final_df()
-    
+        Returns:
+        - cagr (pd.DataFrame): DataFrame with CAGR values for each symbol
+        """
+        return CAGR(self.data_df_returns, self.date_column, self.return_column, self.symbol_column).final_df()
+
     def get_cagr_for_each_quintile_ROIC(self):
+        """
+        Calculate the average CAGR for each quintile based on ROIC.
 
+        Returns:
+        - cagr_quintile (dict): Dictionary where keys represent quintiles and values are the average CAGR for each quintile
+        """
         quintile_symbols = self.get_symbols_in_each_quintile_ROIC()
         cagr = self.get_cagr()
-        
+
         cagr_quintile = {}
         for quintile, symbols in quintile_symbols.items():
             cagr_quintile[quintile] = cagr[cagr[self.symbol_column].isin(symbols)][['cagr']].mean().values[0]
 
         return cagr_quintile
-    
+
     def get_cagr_for_universe(self):
-            
+        """
+        Calculate the average CAGR for the entire universe.
+
+        Returns:
+        - cagr_universe (float): Average CAGR for the entire universe
+        """
         cagr = self.get_cagr()
         return cagr[['cagr']].mean().values[0]
+
 
 
 
